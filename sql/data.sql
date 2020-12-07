1fac34fc8c4529b11d7f7e0800c068958ab62443
tung1207 <32309247+tung1207@users.noreply.github.com>
2020-12-08 01:49:04 +0700

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -5,39 +5,38 @@ use WebCSDL
 
 select * from Product
 
--- Pizza
-INSERT INTO Product
-VALUES 
-('PZ01',N'PIZZA BÒ THƯỢNG HẠNG','299000',N'Vừa Bò viên kiểu Ý, vừa thịt bò thượng hạng. Cắn một miếng thôi là cảm nhận được vị mềm ngọt!','pizza1','Pizza','6'),
-('PZ02',N'PIZZA HẢI SẢN KIỂU SINGAPORE','145000',N'Hải sản phải cay thì mới ngon. Chính là bữa tiệc kiểu Singapore đậm vị!!!','pizza2','Pizza','7'),
-('PZ03',N'PIZZA HẢI SẢN NHIỆT ĐỚI','250000',N'Thưởng thức ngay liều Pizzamin Sea để được "ngập ngụa" trong 3 tầng hương vị topping sảng khoái.','pizza3','Pizza','8'),
-('PZ04',N'1/2 PIZZA HẢI SẢN SỐT MAYONNAISE + 1/2 PIZZA GÀ PHÔ MAI','169000',N'Vừa có gà phô mai lại có thêm thịt heo muối mặn mặn ăn không biết ngán. Bàn tiệc đa dạng topping, sao không thử?!
-','pizza4','Pizza','0'),
-('PZ05',N'PIZZA GÀ SỐT TƯƠNG KIỂU NHẬT','230000',N'Là tín đồ pizza, sao không tận hưởng hương vị độc đáo & tinh tế, pha trộn giữa 2 nền ẩm thực gói gọn trong những miếng bánh ngay hôm nay!!
-!','pizza5','Pizza','15'),
-('PZ06',N'PIZZA XÚC XÍCH PEPPERONI','129000',N'Hương vị truyền thống của đất nước Ý, hương vị pizza ngất ngây đã qua bao thế hệ, nhưng vẫn không hề mất điểm trong lòng những fan cứng của pizza','pizza6','Pizza','0'),
-('PZ07',N'PIZZA BÒ & TÔM','155000',N'Vừa sang vừa ngon, vừa đã mắt vừa đã miệng, bữa tiệc Thịt Bò phối Hải Sản tươi - không thể tuyệt vời hơn!!','pizza7','Pizza','0'),
-('PZ08',N'PIZZA BÁNH MỲ','129000',N'Sự kết hợp độc đáo của biểu tượng phong cách Ý và biểu tượng ẩm thực Việt được yêu thích trên toàn thế giới','pizza8','Pizza','5'),
-('PZ09',N'PIZZA TÁO AN LÀNH','99000',N'Với những lát táo tươi trên nền sốt Caramel ngọt ngào và bột quế thơm nồng','pizza9','Pizza','7'),
-('PZ10',N'PIZZA HẢI SẢN PESTO XANH','169000',N'Là sự kết hợp hoàn hảo giữa tôm, cua, mực và bông cải xanh tươi ngon trên nền sốt Pesto Xanh đậm vị đặc trưng của phong cách ẩm thực miền bắc nước Ý','pizza10','Pizza','0')
-
--- Hamburger
+-- Áo nữ
 INSERT INTO Product
 VALUES
-('HBG08',N'BURGER PHI LÊ GÀ ĐẶC BIỆT','76000',N'Nhiều lớp gà xen kẽ nhau kết hợp với sốt đặc trưng tạo nên hương vị không thể bỏ lỡ <3','hbg8','Hamburger','5'),
-('HBG09',N'BURGER PHÔ MAI','46000',N'Được tạo nên từ phô mai với công thức đặc biệt suốt hằng trăm năm của đất nước Pháp không thể bỏ lỡ!!!','hbg9','Hamburger','7'),
-('HBG10',N'BURGER SIÊU CAY','108000',N'Ớt tươi, mù tạt, tiêu, tỏi, gừng, vân vân và mây mây gia vị. Bạn có dám thử ???','hbg10','Hamburger','2')
+('
+ ATL0446',N'Áo thun giữ nhiệt nữ Emspo cổ 6F dáng suông vừa trắng','229000',N'Áo thun giữ nhiệt nữ được Emspo chăm chút từ khâu thiết kế đến khâu gia công với mong muốn đem đến một sản phẩm tốt nhất cho khách hàng của mình','hbg8','Áo nữ','5'),
+('TC2018002WH',N'Áo thun len tay dài cổ tròn dáng suông The Cosmo (trắng)','99000',N'Áo thun len tay dài, cổ tròn, dáng suông, có lớp lót thun mỏng phía trong. Áo được may trên chất liệu mềm mịn và thoáng mát','hbg9','Áo nữ','7'),
+('14109V620',N'Áo sơ mi kiểu TosonFashion cổ đức xếp ly gấu thắt nơ ren','149000',N'Áo sơ mi kiểu TosonFashion cổ đức xếp ly gấu thắt nơ ren được may từ chất liệu Chiffon đem đến hình ảnh một cô nàng dịu dàng và nữ tính cho phái đẹp. Áo sơ mi kiểu TosonFashion thiết kế cổ đức, thắt nơ eo, mang đến hình ảnh dịu dàng nữ tính cùng cảm giác thoải mái khi mặc','hbg10','Áo nữ','2')
 
--- Milk tea
+-- Áo khoác và áo len nữ
 INSERT INTO Product
 VALUES 
-('MT01',N'SỮA CARAMEL MACCHIATO','78000',N'Sữa caramel macchiato','milktea1','Milktea','25'),
-('MT02',N'SỮA MATCHA MACCHIATO','58000',N'Sữa matcha macchiato','milktea2','Milktea','33'),
-('MT03',N'SINH TỐ DƯA HẤU','46000',N'Sinh tố dưa hấu','milktea3','Milktea','10'),
-('MT04',N'NƯỚC CHANH SẢ MẬT ONG','42000',N'Nước chanh sả mật ong','milktea4','Milktea','5'),
-('MT05',N'XOÀI DỪA ĐÁ XAY','69000',N'Xoài Dừa Đá Xay','milktea5','Milktea','21'),
-('MT06',N'VIỆT QUẤT ĐÁ XAY ','59000',N'Việt Quất Đá Xay','milktea6','Milktea','45'),
-('MT07',N'CHANH TUYẾT','57000',N'Chanh Tuyết','milktea7','Milktea','5'),
-('MT08',N'TRÀ ĐÀO CAM SẢ','64000',N'Trà Đào Cam Sả','milktea8','Milktea','7'),
-('MT09',N'TRÀ TÁO BẠC HÀ','57000',N'Trà Táo Bạc Hà','milktea9','Milktea','29'),
-('MT10',N'TRÀ CHANH CHÉM GIÓ ','46000',N'Trà chanh chém gió','milktea10','Milktea','108')
+('4018',N'Áo khoác nữ cổ chui hoodie Phúc An màu đen','159000',N'Áo khoác 4018 là mẫu áo khoác cổ chui cổ điển không lỗi mốt. Vì không có nút cài hay dây kéo mẫu 4018 trông rất gọn gàng như phong cách minimalist','milktea1','Áo khoác và áo len nữ','25'),
+('V65P18T023-GOS14',N'Áo khoác phao nữ Vienne Tran cổ B túi chéo','299000',N'Áo khoác phao nữ Vienne Tran cổ B túi chéo có chất liệu phao trần bông miếng nhẹ, giữ ấm tốt. Thiết kế áo 2 lớp, có lót. Một chút khéo léo trong khâu mix & match hẳn sẽ giúp bạn trở nên duyên dáng hơn, xinh đẹp hơn trong mắt tất cả mọi người xung quanh','milktea2','Áo khoác và áo len nữ','33'),
+('ATS0370',N'Áo len nữ cổ trụ Emspo bo gấu 2 màu','167300',N'Chất vải được Emspo chọn lọc kỹ lưỡng, thiết kế đơn giản nhưng cũng không kém phần thời trang.','milktea3','Áo khoác và áo len nữ','10'),
+('VT0158 LT185101581',N'Áo Blazer phối lưới thêu hoa Bella Moda','499000',N'Áo Blazer phối lưới thêu hoa Bella Moda với thiết kế trẻ trung, thanh lịch hợp thời trang, giúp cho vẻ ngoài của phái đẹp chỉn chu và lịch sự hơn mỗi khi đến nơi làm việc','milktea4','Áo khoác và áo len nữ','5'),
+('CA20090-22',N'Áo khoác gió nữ Chelle lót lưới bo gấu hồng','265500',N'Áo khoác gió nữ Chelle lót lưới bo gấu Hồng được may từ chất liệu cản gió tốt, khó ngấm nước, là 1 chất liệu tuyệt vời cho mùa thu đông, mang đến sự tiện ích mà vẫn nhẹ nhàng cho người mặc','milktea5','Áo khoác và áo len nữ','21'),
+('15032',N'Áo khoác da lộn Trali nâu trẻ trung','1150000',N'Áo khoác da lộn Trali nâu trẻ trung là một item thời trang được nhiều nàng yêu thích lựa chọn để diện vào những ngày đặc biệt hay đến những nơi quan trọng. Áo blazer luôn "được lòng" các chị em phụ nữ bởi kiểu dáng hiện đại, thời thượng, không kém phần "sang chảnh" và chẳng hề "kén" người mặc. Bạn cũng có thể làm mới phong cách thời trang mùa đông của mình với áo blazer nữ giúp giữ ấm cơ thể cho những ngày se lạnh!','milktea6','Áo khoác và áo len nữ','45'),
+('ALS0010',N'Áo len mỏng Emspo dáng ôm kẻ ngang','89000',N'Emspo sử dụng chất liệu được lựa chọn kỹ lưỡng nhằm mang đến sự thoải mái, dễ chịu nhất cho người mặc cũng như đem đến những trải nghiệm tuyệt vời cho khách hàng khi lựa chọn sản phẩm','milktea7','Áo khoác và áo len nữ','5'),
+('AVL0555',N'Áo vest nữ cổ V Emspo bẻ ve phần trên túi giả 2 bên','329000',N'Sản phẩm của thời trang Emspo luôn là sự kết hợp hoàn hảo giữa chất liệu, kiểu dáng và màu sắc đem lại cho người mặc cảm giác tự tin - thoải mái nhất dù đi đến bất cứ đâu, công sở, dạo phố hay mặc nhà','milktea8','Áo khoác và áo len nữ','7'),
+('62016E949',N'Áo len cổ đức bổ trụ TosonFashion tay và cổ phối màu','199000',N'Áo len cổ đức bổ trụ TosonFashion tay và cổ phối màu được may từ chất liệu len với kiểu dáng đơn giản trẻ trung, nữ tính. Chúng không những êm ái, mềm mại, giữ ấm tuyệt vời cho cơ thể của bạn mà còn có thiết kế trẻ trung, sành điệu dễ dàng phối hợp với mọi kiểu trang phục khác nhau. Hãy làm sáng bừng tủ đồ của bạn trong mùa thu đông năm nay bằng cách lựa chọn một chiếc áo len đến từ TosonFashion nhé!','milktea9','Áo khoác và áo len nữ','29'),
+('L65P18T032-S1800',N'Áo phao cổ dựng khóa đồng Lamer','199000',N'Áo phao cổ dựng khóa đồng Lamer với chất liệu phao siêu nhẹ, cản gió, giữ ấm tốt. Áo 2 lớp, có lót. Cổ dựng, dáng ngắn, dài tay, giữ ấm tốt. Màu sắc trung tính dễ phối đồ','milktea10','Áo khoác và áo len nữ','108')
+
+-- Áo thun nam
+INSERT INTO Product
+VALUES 
+('ATS0357',N'Áo thun nam tay ngắn Emspo dáng suông in hình trẻ trung','269000',N'Áo thun nam tay ngắn Emspo dáng suông in hình trẻ trung được may từ chất liệu Cotton đem đến hình ảnh trẻ trung năng động cho phái mạnh','pizza1','Áo thun nam','6'),
+('ATS0250',N'Áo thun nam tay ngắn Emspo cổ tròn','239000',N'Áo thun nam tay ngắn Emspo cổ tròn được may từ chất liệu thun cotton, thiết kế đơn giản đem lại cho người mặc cảm giác thoải mái dễ chịu','pizza2','Áo thun nam','7'),
+('TC1021065R3RE',N'Áo thun nam THE COSMO JOY TEE kẻ sọc đỏ','99000',N'Áo thun nam THE COSMO JOY TEE kẻ sọc đỏ cổ tròn, tay ngắn, được may từ chất liệu 100% cotton mang đến độ thấm hút mồ hôi tuyệt đối và cảm giác thoải mái, thoáng mát để các chàng thỏa thích vận động suốt cả ngày','pizza3','Áo thun nam','8'),
+('Ao.Polo.VIC.Ghi',N'Áo thun polo nam Vicci cộc tay màu ghi','150000',N'Áo thun polo nam Vicci cộc tay màu ghi được may từ chất liệu vải thun cao cấp, đường may tỉ mỉ, mang đến độ bền cao cho người sử dụng','pizza4','Áo thun nam','0'),
+('TV11A2029',N'Áo thun nam Polo tay ngắn Hàn Quốc The Shirts Studio','399000',N'Áo thun nam Polo tay ngắn Hàn Quốc The Shirts Studio được may từ chất liệu vải polyester và cotton mềm, mát, thoáng khí, thấm hút mồ hôi tốt','pizza5','Áo thun nam','15'),
+('TC1021077BL',N'Áo thun nam tay dài The Cosmo Austin Polo cổ polo đơn sắc','169000',N'Áo thun nam tay dài The Cosmo Austin Polo cổ polo đơn sắc mang đến vẻ ngoài hiện đại và trẻ trung cho các chàng trai sành điệu với thiết kế cơ bản quen thuộc','pizza6','Áo thun nam','0'),
+('ATL0389',N'Áo thun polo nam Emspo dài tay kẻ sọc','299000',N'Áo thun nam Emspo mang phong cách khỏe khoắn, năng động với chất liệu thun co giãn và thấm hút mồ hôi tốt là 1 lựa chọn không thể thiếu trong tủ đồ mùa thu đông của mọi gia đình','pizza7','Áo thun nam','0'),
+('ATS0295',N'Áo thun nam cổ tròn, tay ngắn, in hoạ tiết Emspo','269000',N'Áo thun Emspo cổ tròn, tay ngắn, in hình trước ngực đơn giản mang phong cách khỏe khoắn, năng động với chất liệu thun co giãn và thấm hút mồ hôi tốt là 1 lựa chọn không thể thiếu trong tủ đồ mùa hè của mọi gia đình','pizza8','Áo thun nam','5'),
+('MBT20111GE',N'Áo thun polo nam Mint Basic trẻ trung - xanh lá cây','215000',N'Áo thun polo trên nền thun cotton mềm mại có độ thấm hút cao, màu sắc trơn dễ dàng phối cùng nhiều kiểu quần khác nhau là gợi ý không thể hoàn hảo hơn cho cuối tuần của bạn','pizza9','Áo thun nam','7'),
+('TC1021074R1WH',N'Áo polo nam THE COSMO RALPH SHIRT trơn màu','189000',N'Ralph Polo Shirt được may từ chất liệu 100% cotton cao cấp, đảm bảo sự thấm hút mồ hôi tối đa cho chàng thêm tự tin trong các hoạt động hàng ngày','pizza10','Áo thun nam','0')
